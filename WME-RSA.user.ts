bfcd97192347f878ac596704a439811a5b852022
Mikhail Veygman <mveygman@gmail.com>
2026-01-11 22:42:32 -0500

    Fix

--- WME-RSA.user.ts
+++ WME-RSA.user.ts
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         WME Road Shield Assistant (BETA ONLY)
-// @namespace    https://greasyfork.org/en/users/286957-skidooguy
+// @namespace    https://greasyfork.org/en/users/1442373-mikhail-veygman-karlsosha
 // @version      2026.01.11.001
 // @description  Adds shield information display to WME
 // @author       SkiDooGuy, jm6087, Karlsosha
